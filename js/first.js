61dd270403a2d1ee9afba39426a7def86d696cb6
Ben Krepp <bkrepp@ctps.org>
2024-01-05 13:18:21 -0500

    Added 5 examples from chart.js website; got them working

--- js/first.js
+++ js/first.js
@@ -33,4 +33,60 @@ function initialize() {
 		}
 	}
 	new Chart(c1, cfg1);
+	
+	// Example 2 - modified from example on website
+	const c2 = document.getElementById('example_2');
+	const cfg2 = {
+	  type: 'line',
+	  data: {
+		datasets: [{
+		  data: [{x: 10, y: 20}, {x: 15, y: 5}, {x: 20, y: 10}]
+		}],
+		labels: [10, 15, 20]
+	  }
+	}
+	new Chart(c2, cfg2);
+
+	// Example 3
+	const c3 = document.getElementById('example_3');
+	const cfg3 = {
+	  type: 'line',
+	  data: {
+		datasets: [{
+		  data: [{x: '2016-12-25', y: 20}, {x: '2016-12-26', y: 10}]
+		}]
+	  }
+	}
+	new Chart(c3, cfg3);
+	
+	// Example 4
+	const c4 = document.getElementById('example_4');
+	const cfg4 = {
+	  type: 'bar',
+	  data: {
+		datasets: [{
+		  data: [{x: 'Sales', y: 20}, {x: 'Revenue', y: 10}]
+		}]
+	  }
+	}
+	new Chart(c4, cfg4);
+	
+	// Example 5
+	const c5 = document.getElementById('example_5');
+	const cfg5 = {
+	  type: 'bar',
+	  data: {
+		datasets: [{
+		  data: [{id: 'Sales', nested: {value: 1500}}, {id: 'Purchases', nested: {value: 500}}]
+		}]
+	  },
+	  options: {
+		parsing: {
+		  xAxisKey: 'id',
+		  yAxisKey: 'nested.value'
+		}
+	  }
+	}
+	new Chart(c5, cfg5);
+	
 } //initialize
