ac21a1b46e5320e898970afe737a7428468d7e51
Ben Krepp <bkrepp@ctps.org>
2024-01-08 15:46:07 -0500

    Removed vestigial code and vestigial (empty) comments

--- js/performance-dashboard-transit-safety.js
+++ js/performance-dashboard-transit-safety.js
@@ -1,27 +1,18 @@
-// URLs for CSV files to be loaded
-var roadwaySafetyURL 	= 'csv/roadway_safety.csv',
-	transitSafetyURL 	= 'csv/transit_safety.csv',
-	bridgeAndPavementURL = 'csv/bridge_and_pavement.csv',
-	tamURL				= 'csv/tam.csv',
-	ttrURL				= 'csv/ttr.csv',
-	cmaqURL				= 'csv/cmaq.csv';
-
-
-// CSV parser for transit safety CSV file
+/// CSV parser for transit safety CSV file
 var ts_mpo_RowConverter = function(d) {
 	return {
 		agency:					d['Agency'],
 		targ_2023_fat:			+d['2023 Target - Fatalities'],	//
-		perf_2019_21_fat:		+d['2019-21 Performance - Fatalities'],	//
+		perf_2019_21_fat:		+d['2019-21 Performance - Fatalities'],	
 		targ_2023_fat_rate:		+d['2023 Target - Fatality Rate'],	//
-		targ_2019_21_fat_rate:	+d['2019-21 Performance - Fatality Rate'],	//
-		targ_2023_inj:			+d['2023 Target - Injuries'],		//
-		perf_2019_21_inj:		+d['2019-21 Performance - Injuries'], //
-		targ_2023_inj_rate:		+d['2023 Target - Injury Rate'], // 
+		targ_2019_21_fat_rate:	+d['2019-21 Performance - Fatality Rate'],	
+		targ_2023_inj:			+d['2023 Target - Injuries'],	
+		perf_2019_21_inj:		+d['2019-21 Performance - Injuries'], 
+		targ_2023_inj_rate:		+d['2023 Target - Injury Rate'], 
 		perf_2019_21_inr_rate:	+d['2019-21 Performance - Injury Rate'],
-		targ_2023_saf:			+d['2023 Target - Safety Events'],	//
+		targ_2023_saf:			+d['2023 Target - Safety Events'],
 		perf_2019_21_saf:		+d['2019-21 Performance - Safety Events'],
-		targ_2023_saf_rate:		+d['2023 Target - Safety Event Rate'], // 
+		targ_2023_saf_rate:		+d['2023 Target - Safety Event Rate'], 
 		perf_2019_21_saf_rate:	+d['2019-21 Performance - Safety Event Rate'],
 		targ_2023_sys:			+d['2023 Target - System Reliability in Miles'],
 		perf_2019_21_sys:		+d['2019-21 Performance - System Reliability in Miles']
