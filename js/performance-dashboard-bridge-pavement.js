fc2c041aaf85d517b6fb3a8b9d636671b345b1d9
Ben Krepp <bkrepp@ctps.org>
2024-01-09 16:22:57 -0500

    Expanded API for bridge condition viz

--- js/performance-dashboard-bridge-pavement.js
+++ js/performance-dashboard-bridge-pavement.js
@@ -17,40 +17,100 @@ var b_and_p_mpo_RowConverter = function(d) {
 
 
 
-function generate_bp_viz(xValues, yValues_state_targ, yValues_state_perf, yValues_mpo_perf, canvas_id, chart_title, xAxis_label, yAxis_label) {
+function generate_bridge_viz(xValues, yValues_state_good, yValues_state_poor, yValues_mpo_good, yValues_mpo_poor,
+                             canvas_id, title, xAxis_label, yAxis_label) {
+								 
+	return; // for now
 	
-} // generate_bp_viz
+	// Generate a line chart for bridge condition data
+	var state_good_dataset = { 	label: 'Percent in Good Condition by Deck Area (State)',
+								backgroundColor: 'rgba(0,255,0,.75)',
+								borderWidth: 1.5,
+								borderColor:  'rgb(8,48,107)',
+								spanGaps: true,
+								data: yValues_state_good
+						};
+	var state_poor_dataset = { 	label: 'Percent in Poor Condition by Deck Area (State)',
+								backgroundColor: 'rgb(255,0,0,.75)',
+								borderWidth: 1.5,
+								borderColor: 'rgb(8,48,107)',
+								spanGaps: true,
+								data: yValues_state_poor
+							};
+	var mpo_good_dataset = 	{ 	label: 'Percent in Good Condition by Deck Area (MPO)',
+								backgroundColor: 'rgba(0,0,255,.75)',
+								borderWidth: 1.5,
+								borderColor: 'rgb(8,48,107)',
+								spanGaps: true,
+								data: yValues_mpo_good
+							};
+	var mpo_poor_dataset = 	{ 	label: 'Percent in Poor Condition by Deck Area (MPO)',
+								backgroundColor: 'rgba(255,165,0,.75)',
+								borderWidth: 1.5,
+								borderColor: 'rgb(8,48,107)',
+								spanGaps: true,
+								data: yValues_mpo_poor
+							};							
+
+	var aDatasets = [ state_good_dataset ]; // , state_poor_dataset, mpo_good_dataset, mpo_poor_dataset ];
+	
+	var ctx = document.getElementById(canvas_id);
+	var cfg = {
+		type: 'line',
+		data: {
+			datasets: aDatasets,
+			labels: xValues
+		},
+		options: {
+			plugins: {
+				title: { display: true,
+				         text: chart_title 
+				}
+			},
+			scales: {
+				x: { title: { display: true,  text: xAxis_label } },
+				y: { title: { display: true,  text: yAxis_label } }
+			}
+		}
+	}
+	new Chart(ctx, cfg);	
+} // generate_bridge_viz
 
 
 function bridge_pavement_viz(bp_state_data, bp_mpo_data) {
-	console.log('bridge_pavement_viz: stubbed-out routine');
+	var canvas_id = '';
+	var title = '';
+	var xAxis_label = 'Year';
+	var yAxis_label = '';
+	var xValues = [ 2020, 2021, 2022, 2023, 2024, 2025 ];
+	var yValues_state_good = [], yValues_state_poor = [], yValues_mpo_good = [], yValues_mpo_poor = [];
+	
+	console.log('Entering bridge_pavement_viz');
+	
+	// Percent of NHS bridges in GOOD or POOR condition
+	var canvas_id = 'bridge-viz';
+	var title = 'Condition of Bridges on NHS Roadways';
+	var xAxis_label = 'Year';
+	var yAxis_label = 'Percent';
+	
+	var bridge_state_good = _.find(bp_state_data, function(o) { return o.perf_meas == 'Percent of NHS bridges by deck area classified as in good condition'; });
+	var bridge_state_poor = _.find(bp_state_data, function(o) { return o.perf_meas == 'Percent of NHS bridges by deck area classified as in poor condition'; });
+	var bridge_mpo_good   = _.find(bp_mpo_data,   function(o) { return o.perf_meas == 'Percent of NHS bridges by deck area classified as in good condition'; });
+	var bridge_mpo_poor   = _.find(bp_mpo_data,   function(o) { return o.perf_meas == 'Percent of NHS bridges by deck area classified as in poor condition'; })
+	
+	                       // baseline == 2020
+	yValues_state_good = [ bridge_state_good.baseline, null, null, bridge_state_good.targ_2023, null, bridge_state_good.targ_2025 ];
+	yValues_state_poor = [ bridge_state_poor.baseline, null, null, bridge_state_poor.targ_2023, null, bridge_state_poor.targ_2025 ];
+	yValues_mpo_good   = [ null, bridge_mpo_good.cond_2021, null, null, null, null ];
+	yValues_mpo_poor   = [ null, bridge_mpo_poor.cond_2021, null, null, null, null ];
+	
+
+	generate_bridge_viz(xValues, yValues_state_good, yValues_state_poor, yValues_mpo_good, yValues_mpo_poor,
+                        canvas_id, title, xAxis_label, yAxis_label);
+	
 	
 	return; // for now
 	
-	// 
-
-
-	
-	// Percent of NHS bridges in GOOD condition
-
-	var bridge_good_state = _.find(bp_state_data, function(o) { return o.perf_meas == 'Percent of NHS bridges by deck area classified as in good condition'; });
-	var bridge_good_mpo   = _.find(bp_mpo_data, function(o) { return o.perf_meas == 'Percent of NHS bridges by deck area classified as in good condition'; });
-	
-	yValues_state = [ bridge_good_state.baseline, null, null, bridge_good_state.targ_2023, null, bridge_good_state.targ_2025 ];
-	yValues_mpo   = [ null, bridge_good_mpo.cond_2021, null, null, null, null ];
-	
-	// generate_bp_viz(xValues, yValues_state, yValues_mpo, mylayout, div_id);
-	
-	
-	// Percent of NHS bridges in POOR condition
-
-	var bridge_poor_state = _.find(bp_state_data, function(o) { return o.perf_meas == 'Percent of NHS bridges by deck area classified as in poor condition'; });
-	var bridge_poor_mpo   = _.find(bp_mpo_data, function(o) { return o.perf_meas == 'Percent of NHS bridges by deck area classified as in poor condition'; });
-	
-	yValues_state = [ bridge_poor_state.baseline, null, null, bridge_poor_state.targ_2023, null, bridge_poor_state.targ_2025 ];
-	yValues_mpo   = [ null, bridge_poor_mpo.cond_2021, null, null, null, null ];
-	
-	// generate_bp_viz(xValues, yValues_state, yValues_mpo, mylayout, div_id);
 	
 
 	// Interstate pavement in GOOD condition
