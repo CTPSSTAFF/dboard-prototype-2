ea0b16dcc1b941090b3227a928b4f771b2db70fd
Ben Krepp <bkrepp@ctps.org>
2024-01-09 14:47:05 -0500

    Bare-bones symbolized map of AM LOTTR

--- js/map.js
+++ js/map.js
@@ -18,53 +18,64 @@ function generate_map_viz(towns, lottr) {
     var interstateRoads = lottr_feats;
 	
 	// The following function to be customized
+/*
 	var tip = d3.tip()
 	  .attr('class', 'd3-tip')
 	  .offset([0, 10])
 	  .html(function(d) {
-	    return d.properties.route_num + "<br>AM LOTTR: " + d.properties.am_lottr + "<br>PM LOTTR: " + d.properties.pm_lottr;
+	    // return "AM LOTTR: " + d.properties.am_lottr + "<br>PM LOTTR: " + d.properties.pm_lottr;
 	  });
+*/
 
 	// SVG Viewport
 	var svgContainer = d3.select("#lottr_map").append("svg")
 		.attr("width", "100%")
 		.attr("height", 600);
 		
-	svgContainer.call(tip);
+	// svgContainer.call(tip);
 
 	// Create Boston Region MPO map with SVG paths for individual towns.
-	var mapcSVG = svgContainer.selectAll(".subregion")
+	var mapcSVG = svgContainer.selectAll(".town")
 		.data(towns_feats)
 		.enter()
 		.append("path")
-			.attr("class", "subregion")
-			// .attr("id", function(d, i) { return d.properties.border_link_id; })
+			.attr("class", "town")
 			.attr("d", function(d, i) {return geoPath(d); })
 			.style("fill", "#ddd")
 			.style("stroke", "#191b1d")
 			.style("stroke-width", "1px")
-			.style("opacity", .1);
+			.style("opacity", .5);
 			
 	var interstateSVG = svgContainer.selectAll(".interstate")
 		.data(interstateRoads)
 		.enter()
 		.append("path")
-			.attr("class", function(d) { return d.tmc;})
+			.attr("id", function(d) { return d.tmc;})
 			.attr("d", function(d, i) { return geoPath(d); })
-			.style("fill", "#ddd")
-			.style("stroke-width", function(d) { return (1/d.properties.am_lottr*5); })
+			// .style("fill", "#ddd")
+			.style("stroke-width", function(d) { return 3; /* return (1/d.properties.am_lottr*5); */ })
 			.style("stroke-linejoin", "round")
 			.style("stroke", function(d) {
-				// return colorScale(d.properties.am_lottr);
-				return '#00ff00';
+				var retval;
+				if (d.properties.am_lottr < 1.5) {
+					console.log('low');
+					retval = '#ff0000';
+				} else {
+					console.log('hi');
+					retval = '#00ff00';
+				}	
+				console.log(d.properties.am_lottr);
+				return retval;
 			})
-			.style("opacity", .2)//function(d) { return (d.properties.am_spd_ix-.5);})
-		.on("mouseenter", function(d) {
-            	tip.show(d);
-            })
-        .on("mouseleave", function(d) {
-            	tip.hide(d);
-	     })			
+			.style("opacity", .5);
+/*			
+	interstateSVG.on("mouseenter", function(d) {
+					tip.show(d);
+					})
+				.on("mouseleave", function(d) {
+					tip.hide(d);
+			});	
+*/			
 	
 	_DEBUG_HOOK  = 1;
 	
