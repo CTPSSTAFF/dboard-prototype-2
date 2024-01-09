73536b6d4b79eafea2aafb4f1cb40f6ee63cb8ce
Ben Krepp <bkrepp@ctps.org>
2024-01-09 13:46:54 -0500

    Un-styled map now renders

--- js/map.js
+++ js/map.js
@@ -4,6 +4,69 @@ var mpo_towns_URL 	= 'json/boston_region_mpo_towns_97.geo.json',
 	
 function generate_map_viz(towns, lottr) {
 	var _DEBUG_HOOK = 0;
+	var towns_feats = towns.features;
+	var lottr_feats = lottr.features;
+	
+	var projection = d3.geoConicConformal()
+						.parallels([41 + 43 / 60, 42 + 41 / 60])
+						.rotate([71 + 30 / 60, -41 ])
+						.scale([28000]) // N.B. The scale and translation vector were determined empirically.
+						.translate([120, 1130]);
+
+	var geoPath = d3.geoPath().projection(projection);
+
+    var interstateRoads = lottr_feats;
+	
+	// The following function to be customized
+	var tip = d3.tip()
+	  .attr('class', 'd3-tip')
+	  .offset([0, 10])
+	  .html(function(d) {
+	    return d.properties.route_num + "<br>AM LOTTR: " + d.properties.am_lottr + "<br>PM LOTTR: " + d.properties.pm_lottr;
+	  });
+
+	// SVG Viewport
+	var svgContainer = d3.select("#lottr_map").append("svg")
+		.attr("width", "100%")
+		.attr("height", 600);
+		
+	svgContainer.call(tip);
+
+	// Create Boston Region MPO map with SVG paths for individual towns.
+	var mapcSVG = svgContainer.selectAll(".subregion")
+		.data(towns_feats)
+		.enter()
+		.append("path")
+			.attr("class", "subregion")
+			// .attr("id", function(d, i) { return d.properties.border_link_id; })
+			.attr("d", function(d, i) {return geoPath(d); })
+			.style("fill", "#ddd")
+			.style("stroke", "#191b1d")
+			.style("stroke-width", "1px")
+			.style("opacity", .1);
+			
+	var interstateSVG = svgContainer.selectAll(".interstate")
+		.data(interstateRoads)
+		.enter()
+		.append("path")
+			.attr("class", function(d) { return d.tmc;})
+			.attr("d", function(d, i) { return geoPath(d); })
+			.style("fill", "#ddd")
+			.style("stroke-width", function(d) { return (1/d.properties.am_lottr*5); })
+			.style("stroke-linejoin", "round")
+			.style("stroke", function(d) {
+				// return colorScale(d.properties.am_lottr);
+				return '#00ff00';
+			})
+			.style("opacity", .2)//function(d) { return (d.properties.am_spd_ix-.5);})
+		.on("mouseenter", function(d) {
+            	tip.show(d);
+            })
+        .on("mouseleave", function(d) {
+            	tip.hide(d);
+	     })			
+	
+	_DEBUG_HOOK  = 1;
 	
 } // generate_map_viz
 
