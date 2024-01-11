284ba2a064f8fd77ac0c176bc6235cdba5211b5f
Ben Krepp <bkrepp@ctps.org>
2024-01-11 13:50:53 -0500

    Implemented interstate pavement condition viz

--- js/performance-dashboard-bridge-pavement.js
+++ js/performance-dashboard-bridge-pavement.js
@@ -49,9 +49,8 @@ function generate_bridge_viz(xValues, yValues_state_good, yValues_state_poor, yV
 								spanGaps: true,
 								data: yValues_mpo_poor
 							};	
-
-	var aDatasets = [ state_good_dataset, state_poor_dataset, mpo_good_dataset, mpo_poor_dataset ];
 	
+	var aDatasets = [ state_good_dataset, state_poor_dataset, mpo_good_dataset, mpo_poor_dataset ];
 	var ctx = document.getElementById(canvas_id);
 	var cfg = {
 		type: 'bar',
@@ -74,6 +73,69 @@ function generate_bridge_viz(xValues, yValues_state_good, yValues_state_poor, yV
 	new Chart(ctx, cfg);	
 } // generate_bridge_viz
 
+function generate_pavement_viz(xValues, yValues_state_good, yValues_state_poor, yValues_mpo_good, yValues_mpo_poor,
+                               canvas_id, chart_title, xAxis_label, yAxis_label) {
+	// Generate a bar chart for pavement condition data
+	// Although Sam suggested a line chart, it doesn't look very effective in practice
+	var state_good_dataset = { 	label: 'Percent of Pavement in Good Condition (State)',
+								backgroundColor: 'rgba(0,255,0,.75)',
+								borderWidth: 1.5,
+								borderColor:  'rgb(8,48,107)',
+								spanGaps: true,
+								data: yValues_state_good
+						};
+	var state_poor_dataset = { 	label: 'Percent of Pavement in Poor Condition (State)',
+								backgroundColor: 'rgb(255,0,0,.75)',
+								borderWidth: 1.5,
+								borderColor: 'rgb(8,48,107)',
+								spanGaps: true,
+								data: yValues_state_poor
+							};
+	var mpo_good_dataset = 	{ 	label: 'Percent of Pavement in Good Condition (MPO)',
+								backgroundColor: 'rgba(0,0,255,.75)',
+								borderWidth: 1.5,
+								borderColor: 'rgb(8,48,107)',
+								spanGaps: true,
+								data: yValues_mpo_good
+							};
+	var mpo_poor_dataset = 	{ 	label: 'Percent of Pavement in Poor Condition (MPO)',
+								backgroundColor: 'rgba(255,165,0,.75)',
+								borderWidth: 1.5,
+								borderColor: 'rgb(8,48,107)',
+								spanGaps: true,
+								data: yValues_mpo_poor
+							};	
+	
+	// Note that there are some metrics for which there is no MPO-specific data. Take this into account
+	var aDatasets = [];
+	if (yValues_mpo_good != null) {
+		var aDatasets = [ state_good_dataset, state_poor_dataset, mpo_good_dataset, mpo_poor_dataset ];
+	} else {
+		var aDatasets = [ state_good_dataset, state_poor_dataset ];
+	}
+	
+	var ctx = document.getElementById(canvas_id);
+	var cfg = {
+		type: 'bar',
+		data: {
+			datasets: aDatasets,
+			labels: xValues
+		},
+		options: {
+			plugins: {
+				title: { display: true,
+				         text: chart_title 
+				}
+			},
+			scales: {
+				x: { title: { display: true,  text: xAxis_label } },
+				y: { title: { display: true,  text: yAxis_label } }
+			}
+		}
+	};
+	new Chart(ctx, cfg);
+} // generate_pavement_viz
+
 
 function bridge_pavement_viz(bp_state_data, bp_mpo_data) {
 	var canvas_id = '';
@@ -83,7 +145,6 @@ function bridge_pavement_viz(bp_state_data, bp_mpo_data) {
 	var xValues = [ 2020, 2021, 2022, 2023, 2024, 2025 ];
 	var yValues_state_good = [], yValues_state_poor = [], yValues_mpo_good = [], yValues_mpo_poor = [];
 	
-	console.log('Entering bridge_pavement_viz');
 	
 	// Percent of NHS bridges in GOOD or POOR condition
 	var canvas_id = 'bridge-viz';
@@ -107,25 +168,28 @@ function bridge_pavement_viz(bp_state_data, bp_mpo_data) {
                         canvas_id, title, xAxis_label, yAxis_label);
 	
 	
+
+	
+	// Interstate pavement in GOOD or POOR condition
+	// NOTE: There is **NO** interstate pavement condition data for the MPO only
+	var canvas_id = 'interstate-pavement-viz';
+	var title = 'Pavement Condition on Interstate Roads';
+	var xAxis_label = 'Year';
+	var yAxis_label = 'Percent';
+	
+	var interstate_state_good = _.find(bp_state_data, function(o) { return o.perf_meas == 'Percent of pavements on the Interstate System in good condition'; });
+	var interstate_state_poor = _.find(bp_state_data, function(o) { return o.perf_meas == 'Percent of pavements on the Interstate System in poor condition'; });
+
+	yValues_state_good = [ interstate_state_good.baseline, null, null, interstate_state_good.targ_2023, null, interstate_state_good.targ_2025 ];
+	yValues_state_poor = [ interstate_state_poor.baseline, null, null, interstate_state_poor.targ_2023, null, interstate_state_poor.targ_2025 ];
+	
+	generate_pavement_viz(xValues, yValues_state_good, yValues_state_poor, null, null, canvas_id, title, xAxis_label, yAxis_label);
+	
+	
+	
+	
+	
 	return; // for now
-	
-	
-
-	// Interstate pavement in GOOD condition
-	var interstate_good_state = _.find(bp_state_data, function(o) { return o.perf_meas == 'Percent of pavements on the Interstate System in good condition'; });    
-	// NOTE: There is **NO** interstate pavement condition data for the MPO only
-	yValues_state = [ interstate_good_state.baseline, null, null, interstate_good_state.targ_2023, null, interstate_good_state.targ_2025 ];		
-	
-	// generate_bp_viz(xValues, yValues_state, null, mylayout, div_id);
-	
-	
-	// Interstate pavement in POOR condition
-
-	var interstate_poor_state = _.find(bp_state_data, function(o) { return o.perf_meas == 'Percent of pavements on the Interstate System in poor condition'; });  
-	// NOTE: There is **NO** interstate pavement condition data for the MPO only
-	yValues_state = [ interstate_poor_state.baseline, null, null, interstate_poor_state.targ_2023, null, interstate_poor_state.targ_2025 ];	
-	
-	// generate_bp_viz(xValues, yValues_state, null, mylayout, div_id);
 	
 
 	// Non-interstate NHS pavement in GOOD condition
