822c4d3642d7015aa4a1c282dac109d223c14402
Ben Krepp <bkrepp@ctps.org>
2024-01-16 15:55:28 -0500

    Added MBTA safety event rate viz

--- js/performance-dashboard-transit-safety.js
+++ js/performance-dashboard-transit-safety.js
@@ -142,6 +142,17 @@ function transit_safety_viz(ts_mpo_data) {
 	
 	generate_mbta_safety_viz(xValues, yValues_bus, yValues_hr, yValues_lr, yValues_pt, canvas_id, title, xAxis_label, yAxis_label);
 	
+	// Transit safety event rate - MBTA
+	canvas_id = 'safety-rate-mbta';
+	title = 'Safety Event Rate';
+	yAxis_label = 'Saftey Event Rate';
+	yValues_bus = [ mbta_bus_fat.targ_2023_saf_rate, mbta_bus_fat.perf_2019_21_saf_rate ];
+	yValues_hr =  [ mbta_hr_fat.targ_2023_saf_rate,  mbta_hr_fat.perf_2019_21_saf_rate ];
+	yValues_lr =  [ mbta_lr_fat.targ_2023_saf_rate,  mbta_lr_fat.perf_2019_21_saf_rate ];
+	yValues_pt =  [ mbta_pt_fat.targ_2023_saf_rate,  mbta_pt_fat.perf_2019_21_saf_rate ];
+	
+	generate_mbta_safety_viz(xValues, yValues_bus, yValues_hr, yValues_lr, yValues_pt, canvas_id, title, xAxis_label, yAxis_label);
+	
 	return; // for now
 } // transit_safety_viz
 
