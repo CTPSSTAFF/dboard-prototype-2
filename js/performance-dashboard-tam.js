b59e90c065e57e1156a12e0c690054f59e1ac8f2
Ben Krepp <bkrepp@ctps.org>
2024-01-18 14:35:06 -0500

    TAM parser: take into account data contains '%' characters

--- js/performance-dashboard-tam.js
+++ js/performance-dashboard-tam.js
@@ -2,14 +2,15 @@
 var tam_mpo_RowConverter = function(d) {
 	// NOTE: These values are now entered in percent, with a trailing '%' character.
 	//       They will probably have to be read as strings and THEN converted to numbers.
-	return {
+	temp =  {
 		pm_or_ac:	d['Performance Measure or Asset Category'],
 		agency:		d['Agency'],
 		mode:		d['Mode'],
-		targ_2023:	+d['2023 Target'],
-		perf_2022:	+d['2022 Performance'],
-		targ_2022:	+d['2021 Performance']
-	}
+		targ_2023:	+(d['2023 Target']replace('%','')),
+		perf_2022:	+(d['2022 Performance']replace('%','')),
+		targ_2022:	+(d['2021 Performance']replace('%',''))
+	};
+	return temp;
 };
 
 
