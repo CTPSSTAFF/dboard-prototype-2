eb38a976162a467d83ff8869e12ba38a40b005ea
Ben Krepp <bkrepp@ctps.org>
2024-01-19 09:31:17 -0500

    Basic structure of driver logic for MBTA in place

--- js/performance-dashboard-tam.js
+++ js/performance-dashboard-tam.js
@@ -13,11 +13,83 @@ var tam_mpo_RowConverter = function(d) {
 	return temp;
 };
 
+function generate_mbta_tam_9_category_viz() {
+	// 9-step color palette from: https://colorbrewer2.org/#type=qualitative&scheme=Paired&n=9
+	// with 0.75 opacity added.
+	//
+	// 'rgba(166,206,227,0.75)'
+	// 'rgba(31,120,180,0.75)'
+	// 'rgba(178,223,138,0.75)'
+	// 'rgba(51,160,44,0.75)'
+	// 'rgba(251,154,153,0.75)'
+	// 'rgba(227,26,28,0.75)'
+	// 'rgba(253,191,111,0.75)'
+	// 'rgba(255,127,0.75)'
+	// 'rgba(202,178,214,0.75)'
+	
+	return; // for now
+} // generate_mbta_tam_viz
 
 function tam_viz(tam_mpo_data) {
 	console.log('Entered tam_viz');
-	return; // for now
+	// Generate bar charts for the TAM data
 	
-	// Generate a bar chart for the TAM data
+	var mbta  = _.filter(tam_mpo_data, function(o) { return  o.agency == 'MBTA'; });
+	var cata  =  _.filter(tam_mpo_data, function(o) { return o.agency == 'CATA'; });
+	var mwrta = _.filter(tam_mpo_data, function(o) { return o.agency == 'MWRTA'; });
+	
+	// MBTA TAM
+	//
+	// MBTA Rolling Stock
+	var ab  = _.find(mbta, function(o) { return o.pm_or_ac == 'Rolling Stock' && o.mode == 'Articulated Buses'; });
+	var b   = _.find(mbta, function(o) { return o.pm_or_ac == 'Rolling Stock' && o.mode == 'Busses'; });
+	var crl = _.find(mbta, function(o) { return o.pm_or_ac == 'Rolling Stock' && o.mode == 'CR Locomotives'; });
+	var crc = _.find(mbta, function(o) { return o.pm_or_ac == 'Rolling Stock' && o.mode == 'CR Passenger Coaches'; });
+	var fb  = _.find(mbta, function(o) { return o.pm_or_ac == 'Rolling Stock' && o.mode == 'Ferry Boats'; });
+	var hr  = _.find(mbta, function(o) { return o.pm_or_ac == 'Rolling Stock' && o.mode == 'Heavy Rail'; });
+	var lr  = _.find(mbta, function(o) { return o.pm_or_ac == 'Rolling Stock' && o.mode == 'Light Rail'; });
+	var vt  = _.find(mbta, function(o) { return o.pm_or_ac == 'Rolling Stock' && o.mode == 'Vintage Trolleys'; });
+	var pt  = _.find(mbta, function(o) { return o.pm_or_ac == 'Rolling Stock' && o.mode == 'Paratransit Vehicles'; });
+	
+	
+	// generate_mbta_tam_9_category_viz ....
+	
+	// MBTA Equipment
+	var equip = _.find(mbta, function(o), return o.mode == 'All Equipment'; });
+	
+	// MBTA Facilities
+	var pp = _.find(mbta, function(o) return o.mode == 'Passenger/Parking'; });
+	var am = _.find(mbta, function(o) return o.mode == 'Admin/Maintenance'; });
+	
+	// MBTA Fixed Guideway
+	
+	
+	// CATA TAM
+	//
+	// CATA Rolling Stock
+	
+	
+	// CATA Equipment
+	
+	
+	// CATA Facilities
+	
+	
+	
+	// MWRTA 
+	//
+	// MWRTA TAM 
+	//
+	
+	// MWRTA Rolling Stock
+	
+	
+	// MWRTA Equipment
+	
+	
+	// MWRTA Facilities
+	
+	
+	return; // for now
 	
 } // tam_viz
